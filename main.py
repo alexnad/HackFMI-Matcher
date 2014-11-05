64886e23b5bd15a9ccf28220527b28de9e2df613
NikiMaslarski <jjniki93@gmail.com>
2014-11-06 01:14:12 +0200

    main prints to console

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from matcher import Matcher
 
 def main():
     read = ReadTeams()
-   # teams = read.read_csv(sys.argv[1])
-    teams = read.read_csv('example.csv')
-    list_of_mentors = get_mentors()
-    matched_teams = Matcher(teams, list_of_mentors)
-    return matched_teams.match_mentors()
+    teams = read.read_csv(sys.argv[1])
+  #  teams = read.read_csv('example.csv')
+    mentor_names = get_mentors()
+    matched_teams = Matcher(teams, mentor_names)
+    list_of_mentors = matched_teams.match_mentors()
 
+    for mentor in list_of_mentors:
+        print('{}:'.format(mentor))
+        for team in mentor.teams:
+            print(team)
+        print()
 
 if __name__ == "__main__":
-    pass
+    main()
