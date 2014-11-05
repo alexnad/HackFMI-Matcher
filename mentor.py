7a63d245630b42787e3e9bc1ba8b6e0b66a52c05
alexnad <alexandernadjarian@gmail.com>
2014-11-05 20:58:31 +0200

    Add mentor.py

---
+++ mentor.py
@@ -0,0 +1,12 @@
+class Mentor:
+    TEAM_LIMIT = 5
+
+    def __init__(self, name):
+        self.name = name
+        self.teams = []
+
+    def add_team(self, team_name):
+        if len(self.teams) < Mentor.TEAM_LIMIT:
+            self.teams.append(team_name)
+            return True
+        return False
